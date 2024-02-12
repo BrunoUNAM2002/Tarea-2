d23f1ed43f7be706d43a74d2135593ebc8d4784a
LDavid16 <90665432+LDavid16@users.noreply.github.com>
2024-02-12 13:48:29 -0600

    Update Tarea2.c

--- Tarea2.c
+++ Tarea2.c
@@ -4,7 +4,6 @@ int main() {
     float num1, num2, resultado;
     char operador;
 
-    // Solicitar al usuario los números y el operador
     printf("Ingrese el primer número: ");
     scanf("%f", &num1);
 
@@ -14,7 +13,7 @@ int main() {
     printf("Ingrese el segundo número: ");
     scanf("%f", &num2);
 
-    // Realizar la operación según el operador ingresado
+
     switch (operador) {
         case '+':
             resultado = num1 + num2;
@@ -26,7 +25,7 @@ int main() {
             resultado = num1 * num2;
             break;
         case '/':
-            // Manejar la división por cero
+            
             if (num2 != 0) {
                 resultado = num1 / num2;
             } else {
@@ -39,7 +38,6 @@ int main() {
             return 1;  // Salir del programa con código de error
     }
 
-    // Mostrar el resultado
     printf("Resultado: %.2f\n", resultado);
 
     return 0;
